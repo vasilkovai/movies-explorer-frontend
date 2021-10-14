cd940776764edce82a65706810dbe470aeafc4b5
Irina Vasilkova <i.vasilkova.v@ya.ru>
2021-10-14 20:20:49 +0300

    fix adress

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,2 +1,2 @@
 export const MOVIES_URL = 'https://api.nomoreparties.co';
-export const MAIN_URL = 'http://localhost:3001';
+export const MAIN_URL = 'https://api.cinemaholic.diploma.nomoredomains.monster';
