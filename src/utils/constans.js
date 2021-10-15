a0bfde2bbf60ca30044544b71886dddc7bb8c67c
Irina Vasilkova <i.vasilkova.v@ya.ru>
2021-10-15 21:38:23 +0300

    fix api

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,3 +1,3 @@
 export const MOVIES_URL = 'https://api.nomoreparties.co';
 export const MOVIES_API = 'https://api.nomoreparties.co/beatfilm-movies';
-export const MAIN_API = 'https://cinemaholic.diploma.nomoredomains.monster';
+export const MAIN_API = 'https://api.cinemaholic.diploma.nomoredomains.monster';
